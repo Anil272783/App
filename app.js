d7cc123bc9af5c61b53881e67e8dae81b7fde7a3
Anil272783 <Anilpro2783@gmail.com>
2026-04-30 18:53:10 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('CI/CD via GitHub Actions 🚀');
+  res.send('CI/CD via GitHub Actionssss 🚀');
 });
 
 app.listen(3000, () => console.log('Running'));
